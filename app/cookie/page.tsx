8bb31a41aae8a87517fee774416d26ad365ad071
digitalengineer07 <nikhil119124@gmail.com>
2026-08-01 01:19:27 +0530

    feat: add cookie, privacy, and security policy pages with responsive layout and custom footer component

--- app/cookie/page.tsx
+++ app/cookie/page.tsx
@@ -453,8 +453,8 @@ export default function CookiePolicy() {
       <div className="hidden lg:block">
         <Footer />
       </div>
-      <div className="block lg:hidden mt-12 sm:mt-16">
-        <FooterMobile compact={true} style={{ paddingTop: '30px', paddingBottom: '10px' }} />
+      <div className="block lg:hidden">
+        <FooterMobile />
       </div>
     </div>
   );
